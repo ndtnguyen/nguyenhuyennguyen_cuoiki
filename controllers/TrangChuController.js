83171b8df087eea5a17ec83cfae4b3c6ec19721f
nguyenhihihi <nguyenhihihi@gmail.com>
2017-06-26 19:37:01 +0700

    Add files via upload
    controler load tất cả thông tin của trang chủ

--- controllers/TrangChuController.js
+++ controllers/TrangChuController.js
@@ -1,14 +1,24 @@
 var express = require('express');
-
+var trangchuRepo = require('../models/trangchuRepo');
 var r = express.Router();
 
 r.get('/', function(req, res) {
-    var vm = {
-    	layoutVM: res.locals.layoutVM
-    };
-    res.render('home/index', vm);
+	
+    
+    trangchuRepo.loadTrangChu()
+        .then(function(data) {
+            res.render('trangchu/index',
+            {
+                layoutVM: res.locals.layoutVM,
+                an: data.anhnen,
+            	top5NhieuNhat:  data.SPNhieu, 
+            	top5CaoNhat: data.SPCao, 
+            	top5SapHet :data.SPHet
+            });
+            
+        });   
+
+      
 });
-
-
 
 module.exports = r;
