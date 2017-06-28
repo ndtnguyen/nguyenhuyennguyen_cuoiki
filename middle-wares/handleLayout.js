2aea9f47b00e7b9029fa12d57e9133e33af1cda6
QHZero <dtphuyen2506@gmail.com>
2017-06-28 20:58:57 +0700

    Add files via upload

--- middle-wares/handleLayout.js
+++ middle-wares/handleLayout.js
@@ -1,18 +1,26 @@
-var q = require('q'),
-    danhmucRepo = require('../models/danhmucRepo');
+var Q = require('q');
+var danhmuc = require('../models/danhmucRepo');
+var cart = require('../models/cart');
 
 module.exports = function(req, res, next) {
     if (req.session.isLogged === undefined) {
         req.session.isLogged = false;
     }
-	q.all([
-    	danhmucRepo.loadAll()
-	]).spread(function(cRows) {
-		res.locals.layoutVM = {
-			danhmuc: cRows,
-			isLogged: req.session.isLogged,
-            		curUser: req.session.user
-		};
-    	next();
+
+    Q.all([
+        danhmuc.loadAll()
+    ]).spread(function(data) {
+        res.locals.layoutVM = {
+            danhmuc: data,
+            isLogged: req.session.isLogged,
+            curUser: req.session.user,
+            //nguoimua: req.session.user.permission === 0, 
+            //nguoiban: req.session.user.permission === 1, 
+            //quantri: req.session.user.permission === 2, 
+            amucdautien: data[0].MaDanhMuc,
+            cartSumQ: cart.sumQ(req.session.cart)
+        };
+        next();
     });
-}
+
+};
